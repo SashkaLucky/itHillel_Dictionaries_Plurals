83890a8aaeee3c9a5ad7ee1540061b0857f5536d
Creepy.daffodil <Alexander26Peshkov@ukr.net>
2023-04-11 12:25:21 +0300

    Dictionaries and plurals

--- src/main/java/coffee/order/CoffeeOrderBoard.java
+++ src/main/java/coffee/order/CoffeeOrderBoard.java
@@ -1,5 +1,4 @@
 package coffee.order;
-
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/coffee/order/Order.java
+++ src/main/java/coffee/order/Order.java
@@ -1,2 +1,30 @@
-package org.example.order;public class Order {
+package coffee.order;
+public class Order {
+    private int orderId;
+    private String customerName;
+
+    public Order(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    @Override
+    public String toString() {
+        return "Order #" + orderId + " for " + customerName;
+    }
 }
